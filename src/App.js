420c2218c24043cca19cb7a219c6a7761dcc32c1
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2023-07-03 14:51:44 -0700

    Misc changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from  'react-router-dom'
-import Home from './pages';
-import SigninPage from './pages/signin';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Home} exact />
+        <Route path="/" component={Home} exact />
         {/* <Route path='/signin' component={SigninPage} exact /> */}
       </Switch>
     </Router>
