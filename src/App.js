3d7accc2361cc658b8aa53c06b634606ed397e8d
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2023-07-03 15:00:16 -0700

    misc changes 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
-import SigninPage from "./pages/signin";
+// import SigninPage from "./pages/signin";
 
 function App() {
   return (
