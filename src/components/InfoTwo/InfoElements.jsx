516f886fa49301fd06caae3c36ae971fe4ffa20d
Vishnu <vishvargops@gmail.com>
2024-09-21 01:50:51 -0700

    Overlapping issue fix

--- src/components/InfoTwo/InfoElements.jsx
+++ src/components/InfoTwo/InfoElements.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const InfoContainer = styled.div`
   background: #0c0c0c;
@@ -10,7 +10,7 @@ export const InfoContainer = styled.div`
   position: relative;
   z-index: 1;
   :before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     right: 0;
@@ -20,7 +20,7 @@ export const InfoContainer = styled.div`
     /* linear-gradient(180deg, rgba(0,0,0,0.2)0%, transparent 100%); */
     z-index: 2;
   }
-`
+`;
 export const InfoBg = styled.div`
   position: absolute;
   top: 0;
@@ -30,14 +30,14 @@ export const InfoBg = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
-`
+`;
 export const VideoBg = styled.video`
   width: 100%;
   height: 100%;
   -o-object-fit: cover;
   object-fit: cover;
   background: #232a34;
-`
+`;
 
 export const InfoWrapper = styled.div`
   display: grid;
@@ -49,62 +49,69 @@ export const InfoWrapper = styled.div`
   margin-left: auto;
   padding: 0 24px;
   justify-content: center;
-`
+`;
 export const InfoRow = styled.div`
   display: grid;
   grid-auto-columns: minmax(auto, 1fr);
   align-items: center;
-  grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
+  grid-template-areas: ${({ imgStart }) =>
+    imgStart ? `'col2 col1'` : `'col1 col2'`};
 
   @media screen and (max-width: 768px) {
-    grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)}
+    grid-template-areas: ${({ imgStart }) =>
+      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
   }
-`
+`;
 export const Column1 = styled.div`
   margin-bottom: 15px;
   padding: 0 15px;
   grid-area: col1;
-`
+`;
 export const Column2 = styled.div`
   margin-bottom: 15px;
   padding: 0 15px;
   grid-area: col2;
-`
+`;
 export const TextWrapper = styled.div`
   max-width: 540px;
   padding-top: 0;
   padding-bottom: 60px;
-`
+`;
 export const TopLine = styled.p`
   color: #02bf47;
   font-size: 36px;
-  line-height: 16px;
+  line-height: 1.2;
   font-weight: 1000;
   letter-spacing: 1.4px;
   text-transform: uppercase;
   margin-bottom: 16px;
-`
+
+  @media screen and (max-width: 768px) {
+    font-size: 28px;
+    line-height: 1.3;
+  }
+`;
 
 export const Heading = styled.h1`
   margin-bottom: 24px;
   font-size: 28px;
   line-height: 1.1;
   font-weight: 600px;
-  color: ${({lightText}) => (lightText ? '#f7f8fa' : '#010606')};
+  color: ${({ lightText }) => (lightText ? "#f7f8fa" : "#010606")};
 
   @media screen and (max-width: 480px) {
     font-size: 32px;
   }
-`
-export const Subtitle  = styled.p`
+`;
+export const Subtitle = styled.p`
   max-width: 440px;
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 24px;
-  color: ${({darkText}) => (darkText ? '#010606' : '#fff')};
-`
+  color: ${({ darkText }) => (darkText ? "#010606" : "#fff")};
+`;
 export const BtnWrap = styled.div`
-  display:flex;
+  display: flex;
   justify-content: flex-start;
 `;
 export const ImgWrap = styled.div`
@@ -122,8 +129,8 @@ export const SocialIcons = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 440px;
-`
+`;
 export const SocialIconLink = styled.a`
   color: #f2f5f5;
   font-size: 54px;
-`
+`;
